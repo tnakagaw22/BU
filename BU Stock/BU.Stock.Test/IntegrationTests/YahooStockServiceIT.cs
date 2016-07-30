c47868bcb4e411673ddb3bb133e1eea6e460bb65
tnaka <tnaka@DESKTOP-6UQKGKO>
2016-07-30 15:06:32 -0400

    Added YahooStockService and its Integration Tests

---
+++ BU Stock/BU.Stock.Test/IntegrationTests/YahooStockServiceIT.cs
@@ -0,0 +1,38 @@
+﻿using BU.Stock.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BU.Stock.Test.IntegrationTests
+{
+    [TestClass]
+    public class YahooStockServiceIT
+    {
+        [TestCategory("Integration")]
+        [TestMethod]
+        public void GetCurrentPrice_WhenInputIsValid_ReturnCurrentPrice()
+        {
+            string tickerSymbol = "MSFT";
+            var stockService = new YahooStockService();
+
+            var result = stockService.GetCurrentPrice(tickerSymbol);
+
+            Assert.IsTrue(result > 0);
+        }
+
+        [TestCategory("Integration")]
+        [TestMethod]
+        public void GetCurrentPrice_WhenInputIsNOTValid_ReturnCurrentPrice()
+        {
+            string tickerSymbol = "test";
+            var stockService = new YahooStockService();
+
+            var result = stockService.GetCurrentPrice(tickerSymbol);
+
+            Assert.IsTrue(result == 0);
+        }
+    }
+}
