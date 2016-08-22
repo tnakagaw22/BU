2bebb20373a3d3fe5c8f0e0105a77cde69286654
tnaka <tnaka@DESKTOP-6UQKGKO>
2016-08-21 23:20:46 -0400

    Added console app and configure NLog

---
+++ BU Stock/BU.Stock.ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using BU.Stock.Service;
+using Nito.AsyncEx;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BU.Stock.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = LogManager.GetLogger("foo");
+            logger.Info("Program started");
+            AsyncContext.Run(() => MainAsync(args));
+        }
+
+        static async void MainAsync(string[] args)
+        {
+            var yahooService = new YahooStockService();
+            await yahooService.GetCurrentPrice("MSFT");
+        }
+    }
+}
