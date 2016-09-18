91bae0c40580d611543f880596442a72de7f0946
tnaka <tnaka@DESKTOP-6UQKGKO>
2016-09-17 21:05:58 -0400

    Created AwsSnsService and implemented PublishMessage method

---
+++ BU Stock/BU.Stock.Service/AwsSnsService.cs
@@ -0,0 +1,30 @@
+﻿using Amazon;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BU.Stock.Service
+{
+    public class AwsSnsService
+    {
+        string accessKey = "AKIAJDXEK2NQFZVS6CCA";
+        string secretAccessKey = "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";
+
+        public bool PublishMessage(string subject, string body, string topicArn)
+        {
+            var sns = new AmazonSimpleNotificationServiceClient(accessKey, secretAccessKey, RegionEndpoint.USWest2);
+
+            PublishRequest publishRequest = new PublishRequest();
+            publishRequest.Subject = subject;
+            publishRequest.Message = body;
+            publishRequest.TopicArn = topicArn;
+
+            PublishResponse result = sns.Publish(publishRequest);
+            return result.HttpStatusCode == System.Net.HttpStatusCode.OK;
+        }
+    }
+}
